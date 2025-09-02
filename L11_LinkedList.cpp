f9c740cac0eaf54d2c026f07ee45ab003ff34f81
JosueMonterrey <josuemr0206@gmail.com>
2025-09-01 20:42:43 -0600

    Añadir L11 con SLL y Skipped List

---
+++ L11_LinkedList.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+class Nodo {
+    private:
+    string nombre;
+    int prioridad;
+    Nodo * next = nullptr;
+
+    public:
+    Nodo(string n, int p) : nombre(n), prioridad(p) { }
+
+    string GetNombre() { return nombre; }
+    int GetPrioridad() { return prioridad; }
+    Nodo * GetNext() { return next; }
+    void SetNext(Nodo * n) { next = n; }
+};
+
+class SingleLinkedList {
+    private:
+    Nodo * head = nullptr;
+
+    public:
+    void Add(string nombre, int prioridad);
+    string Pop();
+};
+
+void SingleLinkedList::Add(string nombre, int prioridad) {
+    Nodo * nuevoNodo = new Nodo(nombre, prioridad);
+    
+    if (head == nullptr) {
+        head = nuevoNodo;
+        return;
+    }
+    
+    if (prioridad > head->GetPrioridad()) {
+        nuevoNodo->SetNext(head);
+        head = nuevoNodo;
+        return;
+    }
+    
+    Nodo * nodoActual = head;
+    while (nodoActual->GetNext() != nullptr && nodoActual->GetNext()->GetPrioridad() >= prioridad) {
+        nodoActual = nodoActual->GetNext();
+    }
+
+    nuevoNodo->SetNext(nodoActual->GetNext());
+    nodoActual->SetNext(nuevoNodo);
+}
+
+string SingleLinkedList::Pop() {
+    if (head == nullptr) {
+        return "";
+    }
+
+    Nodo * popped = head;
+    head = popped->GetNext();
+    string nombre = popped->GetNombre();
+    delete popped;
+    return nombre;
+}
+
+int main() {
+    SingleLinkedList SLL;
+
+    string nombre;
+    int prioridad;
+    while (cin >> nombre) {
+        if (nombre == "V") {
+            cout << SLL.Pop() << endl;
+        }
+        else {
+            cin >> prioridad;
+            SLL.Add(nombre, prioridad);
+        }
+    }
+}
---
+++ L11_SkippedList.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+class Moneda {
+    private:
+    mt19937 gen;
+    int probabilidad;
+
+    public:
+    Moneda(int prob) {
+        random_device rd;
+        gen = mt19937(rd());
+        probabilidad = prob;
+    }
+
+    int Tirar() { return gen() & (probabilidad - 1); }
+};
+
+class Nodo {
+    private:
+    string nombre;
+    int prioridad;
+    Nodo * down = nullptr;
+    Nodo * next = nullptr;
+
+    public:
+    Nodo() : nombre(""), prioridad(-1) { }
+    Nodo(string n, int p) : nombre(n), prioridad(p) { }
+
+    string GetNombre() { return nombre; }
+    int GetPrioridad() { return prioridad; }
+    Nodo * GetDown() { return down; }
+    void SetDown(Nodo * d) { down = d; }
+    Nodo * GetNext() { return next; }
+    void SetNext(Nodo * n) { next = n; }
+};
+
+class SkipList {
+    private:
+    Nodo * head = nullptr;
+    Nodo * AddAux(Nodo * nodoActual, string nombre, int prioridad);
+    Nodo * PopAux(Nodo * nodoActual);
+    Moneda moneda;
+
+    public:
+    SkipList(int probabilidad) : head(new Nodo()), moneda(probabilidad) { }
+    void Add(string nombre, int prioridad);
+    string Pop();
+};
+
+void SkipList::Add(string nombre, int prioridad) {
+    Nodo * ascendido = AddAux(head, nombre, prioridad);
+
+    // overflow de ascendencia
+    if (ascendido != nullptr) {
+        Nodo * nuevoNodo = new Nodo(nombre, prioridad);
+        nuevoNodo->SetDown(ascendido);
+    
+        Nodo * nuevaHead = new Nodo();
+        nuevaHead->SetDown(head);
+        nuevaHead->SetNext(nuevoNodo);
+    
+        head = nuevaHead;
+    }
+}
+
+Nodo * SkipList::AddAux(Nodo * nodoActual, string nombre, int prioridad) {
+    // Moverse lo más a la derecha posible
+    while (nodoActual->GetNext() != nullptr && nodoActual->GetNext()->GetPrioridad() >= prioridad) {
+        nodoActual = nodoActual->GetNext();
+    }
+
+    // Caso: estamos en el último nivel
+    if (nodoActual->GetDown() == nullptr) {
+        Nodo * nuevoNodo = new Nodo(nombre, prioridad);
+        nuevoNodo->SetNext(nodoActual->GetNext());
+        nodoActual->SetNext(nuevoNodo);
+
+        return moneda.Tirar() ? nuevoNodo : nullptr;
+    }
+
+    Nodo * ascendido = AddAux(nodoActual->GetDown(), nombre, prioridad);
+
+    // Caso: el nodo no ascendió más
+    if (ascendido == nullptr) {
+        return nullptr;
+    }
+
+    Nodo * nuevoNodo = new Nodo(nombre, prioridad);
+    nuevoNodo->SetDown(ascendido);
+    nuevoNodo->SetNext(nodoActual->GetNext());
+    nodoActual->SetNext(nuevoNodo);
+
+    return moneda.Tirar() ? nuevoNodo : nullptr;
+}
+
+string SkipList::Pop() {
+    if (head->GetNext() == nullptr) {
+        return "";
+    }
+
+    Nodo * popped = PopAux(head);
+    string nombre = popped->GetNombre();
+    delete popped;
+
+    // Limpiar niveles vacíos
+    while (head->GetNext() == nullptr && head->GetDown() != nullptr) {
+        Nodo * borrarHead = head;
+        head = head->GetDown();
+        delete borrarHead;
+    }
+
+    return nombre;
+}
+
+Nodo * SkipList::PopAux(Nodo * headActual) {
+    if (headActual->GetDown() == nullptr) {
+        Nodo * popped = headActual->GetNext();
+        headActual->SetNext(popped->GetNext());
+        return popped;
+    }
+
+    Nodo * popped = PopAux(headActual->GetDown());
+    Nodo * nextPopped = headActual->GetNext();
+
+    if (nextPopped != nullptr && nextPopped->GetNombre() == popped->GetNombre() && nextPopped->GetPrioridad() == popped->GetPrioridad()) {
+        headActual->SetNext(nextPopped->GetNext());
+        delete nextPopped;
+    }
+
+    return popped;
+}
+
+int main() {
+    SkipList SKL(2);
+
+    string nombre;
+    int prioridad;
+    while (cin >> nombre) {
+        if (nombre == "V") {
+            cout << SKL.Pop() << endl;
+        }
+        else {
+            cin >> prioridad;
+            SKL.Add(nombre, prioridad);
+        }
+    }
+}
